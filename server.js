3a3d2d849f496ccbdad8a7adedb172172661609d
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-11-10 23:08:43 -0500

    set the environment file and set the view engine to ejs

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 require("dotenv").config();
+const path = require("path");
+const ejs = require("ejs");
 
 const PORT = process.env.PORT;
-
+// initialize the app
 const app = express();
+
+app.set("views engine", ejs);
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send(`<h1>Hello World</h1>`);
+  console.log(path);
 });
 
 //  server listener
