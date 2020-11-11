26d30a0ecf85fb768b65338e8a062be3c57a9f8c
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-11-10 23:00:33 -0500

    add initial files for article app

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`<h1>Hello World</h1>`);
+});
+
+//  server listener
+app.listen(PORT, () => {
+  console.log("👻Server is running on port " + PORT);
+});
