c513b992f00053111a6a123e5eaf721f537c2ce5
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-11-14 15:48:30 -0500

    article view needs styling

--- models/staticData.js
+++ models/staticData.js
@@ -10,3 +10,5 @@ const articles = [
     content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur quaerat corporis voluptatum aut accusantium odio exercitationem magni necessitatibus dolorem porro. ris voluptatum aut accusantium odio exercitationem magni necessitatibus dolorem porro. Quae voluptatum a assumenda? Consequatur numquam culpa fuga provident soluta? orem ipsum dolor sit amet consectetur adipisicing elit. Tenetur quaerat corporis voluptatum aut accusantium odio exercitationem magni necessitatibus dolorem porro Quae voluptatum a assumenda? Consequatur numquam culpa fuga provident soluta? orem ipsum dolor sit amet consectetur adipisicing elit. Tenetur quaerat corporis voluptatum aut accusantium odio exercitationem magni necessitatibus dolorem porro. Quae voluptatum a assumenda? Consequatur numquam culpa fuga provident soluta`,
   },
 ];
+
+module.exports = articles;
